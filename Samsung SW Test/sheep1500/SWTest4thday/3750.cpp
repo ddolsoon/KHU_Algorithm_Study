430b5ae92a7030b965fd627ef9967ef53228c595
sheep1500 <cjswotl6274@naver.com>
2018-03-19 09:55:50 +0900

    complete 3750.cpp

--- Samsung SW Test/sheep1500/SWTest4thday/3750.cpp
+++ Samsung SW Test/sheep1500/SWTest4thday/3750.cpp
@@ -1,29 +1,27 @@
-//
-// Created by jameskang on 18. 3. 19.
-//
 #include <iostream>
-#include <string>
+#include <cstring>
 using namespace std;
 
+char array[18];
 int main(){
-
+    ios_base::sync_with_stdio(false);
     int t;
     int c = 0;
-    cin>>t;
+    scanf("%d",&t);
     while(c++<t){
         string f;
-        cin>>f;
-        while(f.length()!=1){
-            int temp=0;
-            for(int i=0;i<f.length();i++){
-                string t;
-                t += f[i];
-                temp += atoi(t.c_str());
+        scanf("%s",array);
+        int temp=10;
+        while(temp>=10){
+            temp=0;
+            for(int i=0;i<array[i]!=NULL;i++){
+                temp+=(int)(array[i]-'0');
             }
-            f = to_string(temp);
+            memset(array,NULL,sizeof(char)* 18);
+            sprintf(array,"%d",temp);
         }
-        cout<<"#"<<c<<" "<<f<<endl;
+
+        printf("#%d %d\n",c,temp);
     }
     return 0;
 }
-
